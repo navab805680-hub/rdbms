4873c28d375ddf0330c8a375ca2857846f894698
navab805680-hub <navab805680@gmail.com>
2026-07-25 13:26:54 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,7 +1,13 @@
 -- Create database if required
 
 -- Create Student table
-
+CREATE TABLE student(
+   studentID INT(5)PRIMARY KEY,
+   studentName VARCHAR(20)NOT NULL,
+   DOB DATE NOT NULL,
+   Gender VARCHAR(10)NOT NULL,
+   DepartmentID INT(50)NOT NULL
+   );
 -- Insert sample records
 
 -- Update Karthik's DepartmentID
