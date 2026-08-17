a6c130d9a7fc2fd69c2d4b09ab53cf4d30346708
navab805680-hub <navab805680@gmail.com>
2026-08-17 12:47:33 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,4 +1,4 @@
--- Create database if required
+ -- Create database if required
 
 -- Create Student table
 CREATE TABLE student(
@@ -19,3 +19,26 @@ VALUES (1001, 'Arun', 'Male', 101),
 -- Delete StudentID 1002
 delete from stud
 -- Display all records
+
+CREATE DATABASE college;
+USE college;
+
+CREATE TABLE student (
+    studentID INT PRIMARY KEY,
+    studentName VARCHAR(50),
+    department_id INT
+);
+
+INSERT INTO student VALUES (1001, 'Arun', 101);
+INSERT INTO student VALUES (1002, 'Kumar', 102);
+INSERT INTO student VALUES (1003, 'Karthik', 103);
+INSERT INTO student VALUES (1004, 'Ravi', 104);
+
+UPDATE student
+SET department_id = 103
+WHERE studentName = 'Karthik';
+
+DELETE FROM student
+WHERE studentID = 1002;
+
+SELECT * FROM student;
